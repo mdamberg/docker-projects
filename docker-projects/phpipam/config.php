2471a6f4bd50efb0e9810d3e134793d890c82a3b
DataDad <mattdamberg@gmail.com>
2025-11-07 12:10:20 -0600

    added config file for php to hold DB settings

---
+++ docker-projects/phpipam/config.php
@@ -0,0 +1,7 @@
+<?php
+/* database connection details */
+$db['host'] = 'phpipam-mariadb';   // <-- service name from compose
+$db['user'] = 'phpipam';           // MYSQL_USER
+$db['pass'] = 'phppass';           // MYSQL_PASSWORD
+$db['name'] = 'phpipam';           // MYSQL_DATABASE
+$db['port'] = 3306;
