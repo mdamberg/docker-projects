ce19acddb1836e8548a430b78fe2621cb135ec9b
DataDad <mattdamberg@gmail.com>
2026-01-17 21:48:25 -0500

    added dim date model, set day of week to 1 -7 with 1 being Monday and 7 being Sunday.

---
+++ docker-projects/home_metrics_dbt/models/marts/dim/dim_date.sql
@@ -0,0 +1,41 @@
+{{ config(
+    materialized='table',
+    schema='marts'
+)}}
+
+WITH date_spine AS (
+      SELECT generate_series(
+          '2020-01-01'::date,
+          '2030-12-31'::date,
+          '1 day'::interval
+      )::date AS date_day
+  ),
+  dim_date_cte as (
+  SELECT
+        to_char(date_day, 'YYYYMMDD')::int AS date_id,
+      date_day,
+      to_char(date_day, 'Day') AS day_of_week,
+       (extract(ISODOW FROM date_day))::smallint AS day_of_week_num,
+      date_trunc('month', date_day)::date AS month_start_date,
+      (date_trunc('month', date_day) + interval '1 month' - interval '1 day')::date AS month_end_date,
+      to_char(date_day, 'Month') AS month_name,
+      extract(MONTH FROM date_day)::smallint AS month_number,
+      date_trunc('year', date_day)::date AS year_start_date,
+      extract(YEAR FROM date_day)::int AS year_num,
+      extract(DOW FROM date_day) IN (0, 6) AS is_weekend
+    from date_spine
+  )
+
+SELECT
+    date_id,
+    date_day,
+    day_of_week,
+    day_of_week_num,
+    month_start_date,
+    month_end_date,
+    month_name,
+    month_number,
+    year_start_date,
+    year_num,
+    is_weekend
+FROM dim_date_cte
---
+++ docker-projects/home_metrics_dbt/models/marts/dim/dim_hardware_entities.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized='table',
+    schema='marts'
+) }}
+
+with hardware_objects as (
+
+    select
+        distinct {{ dbt_utils.generate_surrogate_key(['hostname', 'sensor_name', 'sensor_id','id']) }} as id,
+        case when hostname = 'DESKTOP-QGA3DvB' then 'server_desktop' else hostname end as hostname,
+        sensor_type,
+        sensor_name,
+        sensor_id,
+        hardware_name,
+        hardware_type
+    from {{ ref('stg_hardware_sensors') }}
+)   
+select
+    *
+from hardware_objects
