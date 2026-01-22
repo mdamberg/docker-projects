db4d5e9d075c18d0415369815620a950f0908d23
DataDad <mattdamberg@gmail.com>
2026-01-21 21:54:02 -0500

    refactored the stg hardware sensors table by adding skeys at a table grain, sensor and host level for down stream joins

--- docker-projects/home_metrics_dbt/models/staging/stg_hardware_sensors.sql
+++ docker-projects/home_metrics_dbt/models/staging/stg_hardware_sensors.sql
@@ -4,40 +4,33 @@
     )   }}
 
 
-with hardware_sensors as (
+with src as (
     select
         hostname,
         sensor_type,
         sensor_name,
+        sensor_id,
+        hardware_name,
+        hardware_type,
         value::float as sensor_value,
-        recorded_at::date as recorded_date,
-        date_trunc('second', recorded_at) as recorded_at_ts,
-        inserted_at::date as inserted_date,
-        inserted_at as inserted_at_ts
+        unit,
+        recorded_at,                               -- keep full precision
+        date_trunc('second', recorded_at) as recorded_at_ts,  -- optional convenience
+        inserted_at
     from {{ source('home_metrics_raw', 'raw_hardware_sensors') }}
-),
-hardware_pivot as (
-    select
-        hostname,
-        recorded_date,
-        recorded_at_ts,
-        inserted_date,
-        max(inserted_at_ts) as inserted_at_ts,
-
-        max(case when sensor_type = 'temperature' and sensor_name = 'Core (Tctl/Tdie)' then sensor_value end) as cpu_core_temp,
-        max(case when sensor_type = 'clock'       and sensor_name = 'Cores (Average)'  then sensor_value end) as cpu_clock_speed,
-        max(case when sensor_type = 'load'        and sensor_name = 'CPU Total'        then sensor_value end) as cpu_load,
-        max(case when sensor_type = 'clock'       and sensor_name = 'GPU Core'         then sensor_value end) as gpu_clock_speed,
-        max(case when sensor_type = 'load'        and sensor_name = 'GPU Core'         then sensor_value end) as gpu_usage
-
-    from hardware_sensors
-    group by
-        hostname,
-        recorded_date,
-        recorded_at_ts, 
-        inserted_date
-
 )
-select *
-from hardware_pivot
-order by recorded_at_ts desc
+
+select
+    {{ dbt_utils.generate_surrogate_key(['hostname']) }} as host_key,
+    {{ dbt_utils.generate_surrogate_key(['hostname','sensor_id']) }} as sensor_key,
+
+    -- pick ONE of these depending on your desired grain:
+    {{ dbt_utils.generate_surrogate_key(['hostname','sensor_id','recorded_at']) }} as reading_key,
+    hostname,
+    sensor_type,
+    sensor_name,
+    sensor_id,
+    sensor_value,
+    hardware_name,
+    hardware_type
+from src
