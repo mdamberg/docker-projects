ed0091a1c04507a5813a865e2d93e8f94d391964
DataDad <mattdamberg@gmail.com>
2026-04-24 17:09:10 -0400

    added date key to intmdt_transactions

--- docker-projects/home_metrics_dbt/models/staging/transactions/stg_teller_balances.sql
+++ docker-projects/home_metrics_dbt/models/staging/transactions/stg_teller_balances.sql
@@ -4,12 +4,13 @@
 ) }}
 
 
-select 
+select
     id as balance_pk,
     teller_account_id,
     {{ dbt_utils.generate_surrogate_key(['teller_account_id']) }} as account_key,
+    to_char(recorded_at, 'YYYYMMDD')::integer as date_key,
     ledger_balance,
-    available_balance, 
+    available_balance,
     recorded_at,
     inserted_at
 from {{ source('home_metrics_raw', 'raw_teller_balances') }}
--- docker-projects/home_metrics_dbt/models/staging/transactions/stg_teller_transactions.sql
+++ docker-projects/home_metrics_dbt/models/staging/transactions/stg_teller_transactions.sql
@@ -8,8 +8,9 @@ select
     {{ dbt_utils.generate_surrogate_key(['id']) }} as transaction_pk,
     teller_transaction_id,
     teller_account_id,
-    -- dimension key: unique to eaach account, but not unique across accounts
+    -- dimension key: unique to each account, but not unique across accounts
     {{ dbt_utils.generate_surrogate_key(['teller_account_id']) }} as account_key,
+    to_char(transaction_date, 'YYYYMMDD')::integer as date_key,
     transaction_date,
     description as transaction_description,
     amount as transaction_amount,
